08c9199c1c17ca0c6e2d8d7ff018662d9d0f65f4
gabrielimartins <martins.gabrieli.07.almeida@outlook.com>
2023-11-10 16:28:46 -0300

    Criando a estrutura base do projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, View } from 'react-native';
+
+import { styles } from './styles';
+import { Button } from './components/Button';
 
 export default function App() {
+  const [selectedImageUri, setSelectedImageUri] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar 
+        style="light"
+        backgroundColor="transparent"
+        translucent 
+      />
+
+      <Image 
+        source={{ uri: selectedImageUri ? selectedImageUri: 'https://teddytennis.com/usa/wp-content/uploads/sites/88/2017/11/placeholder.png' }} 
+        style={styles.image}
+      />
+      <Button title="Selecionar imagem" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#171717',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    image: {
+        width: 250,
+        height: 250,
+        borderRadius: 8,
+    }
+  });
